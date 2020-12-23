57dbbfbd2ae9f485b49a84d1d4e381dd788fcfaf
Zernach <Ryan@Zernach.com>
2020-12-23 12:54:36 -0500

    tests node js require dotenv

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,4 @@
-const API_KEY="pk.eyJ1IjoiYWRyaWFuYW92YWxsZSIsImEiOiJja2VhbDlramYwMXFmMnNzMTdsajM1YTFmIn0.gaUSSuEJmHaoEjlvzyy4sQ";
+//const API_KEY="pk.eyJ1IjoiYWRyaWFuYW92YWxsZSIsImEiOiJja2VhbDlramYwMXFmMnNzMTdsajM1YTFmIn0.gaUSSuEJmHaoEjlvzyy4sQ";
+require("dotenv").config({
+    API_KEY: `.env.${process.env.API_KEY}`,
+  })
